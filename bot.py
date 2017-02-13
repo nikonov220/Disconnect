0c64974a69df5c152cd90856a0ec8dabb6620ebb
nikonov220 <nikonov220@gmail.com>
2017-02-14 01:24:02 +0300

    Admins can add to editor's pick stream

--- bot.py
+++ bot.py
@@ -33,10 +33,31 @@ def set_username(message):
                 models.User.get(models.User.uid == message.chat.id).username))
 
 
+@bot.message_handler(commands=['add_music'])
+def add_music(message):
+
+    try:
+        admin = models.User.get(models.User.uid == message.chat.id).admin
+    except models.DoesNotExist:
+        bot.send_message(message.chat.id, "You have no permanent account\n/create will get you one!")
+        return 1
+    try:
+        content = message.text.split()[1:]
+        content = "".join(content)
+    except IndexError:
+        bot.send_message(message.chat.id, "It's /add_music <soundcloud_iframe>")
+        return 1
+
+    if admin:
+        models.EditorPick.create(cat='1',
+                                 content=content)
+        bot.send_message(message.chat.id, "Added your post!")
+
+
 @bot.message_handler(commands=['me'])
 def me(message):
     try:
-        username = models.User.get(models.User.uid == message.chat.id).username
+        username = models.User.get(models.User.uid == message.chat.id).uid
     except models.DoesNotExist:
         bot.send_message(message.chat.id, "You have no permanent account\n/create will get you one!")
     else:
@@ -44,12 +65,6 @@ def me(message):
             bot.send_message(message.chat.id, "'{}'".format(username))
         else:
             bot.send_message(message.chat.id, "No username was set")
-
-
-@bot.message_handler(commands=['memes'])
-def memes(message):
-    # bot.send_message()
-    pass
 
 
 @bot.message_handler(func=lambda m: True)
